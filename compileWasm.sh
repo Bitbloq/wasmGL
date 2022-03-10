581722416274c32ee1de7ab598932e47925813c8
Alberto Valero <alberto.valero@bq.com>
2022-03-10 18:19:20 +0100

    updated compile wasm

--- compileWasm.sh
+++ compileWasm.sh
@@ -6,6 +6,6 @@ docker run \
   -v $(pwd):/src \
   -u $(id -u):$(id -g) \
   emscripten/emsdk \
-  mkdir wasmbuild
-  emcc -std=c++17 ./src/*.cpp -s USE_GLFW=3 -s FULL_ES3=1 -s GL_ASSERTIONS=1 -s GL_DEBUG=1  -s WASM=1 -o wasmbuild/main.html
+  [ ! -d "./wasmbuild" ] && mkdir wasmbuild
+  emcc -std=c++17 ./src/*.cpp ./src/primitives/*.cpp ./src/window/*.cpp ./src/shaders/*.cpp -I./src/include -s USE_GLFW=3 -s FULL_ES3=1 -s GL_ASSERTIONS=1 -s GL_DEBUG=1  -s WASM=1 -o wasmbuild/main.html
   emrun --port=8080 wasmbuild/main.html
